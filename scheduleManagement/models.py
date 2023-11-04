117a9726f521a508e5fa17ff894615f746dd24bf
Carlos L. ✔ <lopes.carlos.host@gmail.com>
2023-11-04 11:03:08 -0300

    Starting project

---
+++ scheduleManagement/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+class Horario(models.Model):
+  dia = models.DateField()
+  horarioInicio = models.TimeField()
+  horarioTermino = models.TimeField()
+  
+  def __str__(self):
+    return f'{self.dia} | {self.horarioInicio} - {self.horarioTermino}'
+
+class Disciplina(models.Model):
+  nome = models.CharField(max_length=255)
+  codigo = models.CharField(max_length=255)
+  horario = models.OneToOneField(Horario, on_delete=models.CASCADE)
+  
+  def __str__(self):
+    return self.nome
+  
+class Curso(models.Model):
+  nome = models.CharField(max_length=255)
+  codigo = models.CharField(max_length=255)
+  disciplinas = models.ManyToManyField(Disciplina)
+  horario = models.OneToOneField(Horario, on_delete=models.CASCADE)
+  
+  def __str__(self):
+    return self.nome
+  
