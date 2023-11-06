65875bac126fa388a8cd22e9ad4f32c063a3bf86
Carlos L. ✔ <lopes.carlos.host@gmail.com>
2023-11-06 17:42:36 -0300

    (feat): create request handlers

--- scheduleManagement/views.py
+++ scheduleManagement/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Curso, Disciplina
 
 # Create your views here.
+def main_request_handler(request):
+  return render(request, 'index.html', {'cursos': Curso.objects.all()})
+
+def details_request_handler(request, curso_id):
+  return render(request, 'details.html', {'curso': Curso.objects.get(id=curso_id)})
+
+def disciplina_request_handler(request, disciplina_id):
+  return render(request, 'disciplina.html', {'disciplina' : Disciplina.objects.get(id=disciplina_id)})
