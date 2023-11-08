e86a9fbb6720dd7404e110eaf3e120fa2967f11b
LopesLs <lopes.carlos.host@gmail.com>
2023-11-08 20:22:00 -0300

    (refactor): FBV to CBV

--- scheduleManagement/urls.py
+++ scheduleManagement/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.main_request_handler, name='main'),
-  path('details/<int:curso_id>', views.details_request_handler, name='curso_details'),
-  path('disciplina/<int:disciplina_id>', views.disciplina_request_handler, name='disciplina_details')
+  path('', views.main_request_handler.as_view(), name='main'),
+  path('details/<int:curso_id>', views.details_request_handler.as_view(), name='curso_details'),
+  path('disciplina/<int:disciplina_id>', views.disciplina_request_handler.as_view(), name='disciplina_details')
 ]
--- scheduleManagement/views.py
+++ scheduleManagement/views.py
@@ -1,13 +1,16 @@
+from django.views import View
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Curso, Disciplina
 
 # Create your views here.
-def main_request_handler(request):
-  return render(request, 'index.html', {'cursos': Curso.objects.all()})
+class main_request_handler(View):
+  def get(self, request):
+    return render(request, 'index.html', {'cursos': Curso.objects.all()})
 
-def details_request_handler(request, curso_id):
-  return render(request, 'details.html', {'curso': Curso.objects.get(id=curso_id)})
+class details_request_handler(View):
+  def get(self, request, curso_id):
+    return render(request, 'details.html', {'curso': Curso.objects.get(id=curso_id)})
 
-def disciplina_request_handler(request, disciplina_id):
-  return render(request, 'disciplina.html', {'disciplina' : Disciplina.objects.get(id=disciplina_id)})
+class disciplina_request_handler(View):
+  def get(self, request, disciplina_id):
+    return render(request, 'disciplina.html', {'disciplina' : Disciplina.objects.get(id=disciplina_id)})
