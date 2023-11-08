486bac14e65c4ce0030384eacc8712fa3d99bc0e
LopesLs <lopes.carlos.host@gmail.com>
2023-11-08 20:02:16 -0300

    (feat): verbose name added

--- scheduleManagement/apps.py
+++ scheduleManagement/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class SchedulemanagementConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'scheduleManagement'
+    verbose_name = 'Controle de Horários'
