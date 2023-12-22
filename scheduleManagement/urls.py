fb1a3334864abf45906d8b80e8e50c9d22e9556c
LopesLs <97265556+LopesLs@users.noreply.github.com>
2023-12-22 19:08:32 -0300

    (refactor): re-writing some code

--- scheduleManagement/urls.py
+++ scheduleManagement/urls.py
@@ -3,8 +3,10 @@ from . import views
 
 urlpatterns = [
   path('', views.MainRequestHandler.as_view(), name='home'),
-  path('details/<int:curso_id>', views.DetailsRequestHandler.as_view(), name='curso_details'),
+  
   path('disciplina/<int:disciplina_id>', views.DisciplinaRequestHandler.as_view(), name='disciplina_details'),
+  
   path('add-course/', views.CourseAddRequestHandler.as_view(), name='add_course'),
+  path('details/<int:curso_id>', views.DetailsRequestHandler.as_view(), name='curso_details'),
   path('edit-course/<int:curso_id>', views.CourseEditRquestHandler.as_view(), name='edit_course'),
 ]
