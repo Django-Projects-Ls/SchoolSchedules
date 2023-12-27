4156907f38d1945316633117e67c45303cdc0ca9
LopesLs <97265556+LopesLs@users.noreply.github.com>
2023-12-27 17:41:06 -0300

    (feat): implement imports

---
+++ scheduleManagement/views/__init__.py
@@ -0,0 +1,29 @@
+from .create import (
+    CourseCreateRequestHandler,  # noqa: F401
+    DisciplineCreateRequestHandler,  # noqa: F401
+    ScheduleCreateRequestHandler,  # noqa: F401
+)
+
+from .list import (
+    CourseListRequestHandler,  # noqa: F401
+    DisciplineListRequestHandler,  # noqa: F401
+    ScheduleListRequestHandler,  # noqa: F401
+)
+
+from .update import (
+    CourseUpdateRequestHandler,  # noqa: F401
+    DisciplineUpdateRequestHandler,  # noqa: F401
+    ScheduleUpdateRequestHandler,  # noqa: F401
+)
+
+from .details import (
+    CourseDetailsRequestHandler,   # noqa: F401
+    DisciplineDetailsRequestHandler,  # noqa: F401
+    ScheduleDetailsRequestHandler,  # noqa: F401
+)
+
+from .delete import (
+    CourseDeleteRequestHandler,  # noqa: F401
+    DisciplineDeleteRequestHandler,  # noqa: F401
+    ScheduleDeleteRequestHandler,  # noqa: F401
+)
