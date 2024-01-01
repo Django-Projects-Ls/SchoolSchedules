c8d5b55af7c5208060edf8197a2a1d67ca7353db
LopesLs <97265556+LopesLs@users.noreply.github.com>
2024-01-01 18:34:19 -0300

    (feat): Add the authentication with google

--- schoolschedules/urls.py
+++ schoolschedules/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('scheduleManagement.urls')),
     path('', include('django.contrib.auth.urls')),
+    path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
 ]
