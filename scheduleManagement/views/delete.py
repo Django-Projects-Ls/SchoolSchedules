a6d9a7e0b44102b7e5c886b80fffd9442365b38b
LopesLs <97265556+LopesLs@users.noreply.github.com>
2024-01-14 20:04:05 -0300

    (fix): fix auth problem

--- scheduleManagement/views/delete.py
+++ scheduleManagement/views/delete.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
 from django.views.generic.edit import DeleteView
 from django.contrib.auth.models import User
 from django.urls import reverse_lazy
@@ -10,6 +11,18 @@ class UserDeleteRequestHandler(LoginRequiredMixin, DeleteView):
     model = User
     template_name = "confirm-delete.html"
     success_url = reverse_lazy("home")
+
+    def get_object(self, queryset=None):  # noqa: F821
+        """ Only let the user delete their own account """
+        
+        object_instance = super().get_object()
+        
+        if object_instance != self.request.user:
+            print(object_instance, self.request.user)
+            raise PermissionDenied()
+        
+        print(object_instance, self.request.user)
+        return object_instance
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
